affe1e290e02a154162f5020a529e005f4ca4f80
0871063 <c0871063@mylambton.ca>
2022-10-26 19:56:34 -0400

    Initial Commit

---
+++ Malsha_C0871063_Note_P1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Malsha_C0871063_Note_P1
+//
+//  Created by Malsha Lambton on 2022-10-26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
